8835dfd8d0407b2912352d4d01e57568a8742db9
Prince Kongo <pkkongo@st.ug.edu.gh>
2023-07-13 20:10:31 -0700

    finally done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,10 @@
  */
 int main(void)
 {
-    int len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, len);
-    return (1);
+int len = sizeof("and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n") - 1;
+const char *str = "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n";
+write(2, str, len);
+return (1);
 }
