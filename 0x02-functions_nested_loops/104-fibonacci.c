8a3133e2c614044a04af575412c7a4d47aa3f1c4
Prince Kongo <pkkongo@st.ug.edu.gh>
2023-07-19 00:51:33 -0700

    updatated fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,8 +8,9 @@ void add_strings(char result[], const char str1[], const char str2[]) {
     int len1 = strlen(str1);
     int len2 = strlen(str2);
     int max_len = len1 > len2 ? len1 : len2;
+    int i;
 
-    for (int i = 0; i < max_len || carry; i++) {
+    for (i = 0; i < max_len || carry; i++) {
         int digit1 = i < len1 ? str1[len1 - 1 - i] - '0' : 0;
         int digit2 = i < len2 ? str2[len2 - 1 - i] - '0' : 0;
         int sum = digit1 + digit2 + carry;
@@ -31,7 +32,8 @@ int main(void) {
 
     printf("%s, %s", fib1, fib2);
 
-    for (int i = 0; i < 96; i++) {
+    int i;
+    for (i = 0; i < 96; i++) {
         add_strings(temp, fib1, fib2);
         strcpy(fib1, fib2);
         strcpy(fib2, temp);
