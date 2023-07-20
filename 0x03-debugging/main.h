2935e85d1d04a3db8a0988a4de7514d36a7fc6aa
Prince Kongo <pkkongo@st.ug.edu.gh>
2023-07-19 19:42:23 -0700

    first debug

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Prints whether the given integer is positive,
+ *                         negative, or zero.
+ * @i: The integer to be checked.
+ */
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
