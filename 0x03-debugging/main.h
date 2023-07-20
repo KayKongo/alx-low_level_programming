988a341fa0317b5be10acaaa6a4a7f18af563277
Prince Kongo <pkkongo@st.ug.edu.gh>
2023-07-19 19:50:42 -0700

    nice

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,7 +7,14 @@
  * positive_or_negative - Prints whether the given integer is positive,
  *                         negative, or zero.
  * @i: The integer to be checked.
+ *
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
+int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
